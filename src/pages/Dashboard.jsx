d2e596033aeca7a1315aef671e35f22e4f4a74c5
maximebaroz-oss <maxime.baroz@gmail.com>
2026-04-01 17:53:13 +0200

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -880,7 +880,7 @@ export default function Dashboard({ unit, sector, onBack }) {
             )}
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
             <SupervisorCard
               date={selectedDate}
               allProfiles={allProfiles}
